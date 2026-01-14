075ae8ced58d74d7d95a97eacb1434385d8cfd99
Alba Lopez <alba.lopez@in2.es>
2026-01-14 12:35:05 +0100

    logs

--- src/main/java/es/in2/wallet/domain/services/impl/OID4VCICredentialServiceImpl.java
+++ src/main/java/es/in2/wallet/domain/services/impl/OID4VCICredentialServiceImpl.java
@@ -195,7 +195,8 @@ public class OID4VCICredentialServiceImpl implements OID4VCICredentialService {
                             return response.bodyToMono(String.class)
                                     .handle((responseBody, sink) -> {
                                         try {
-                                            System.out.println(responseBody);
+                                            log.info("XIVATO");
+                                            log.info(responseBody);
                                             CredentialResponse credentialResponse =
                                                     objectMapper.readValue(responseBody, CredentialResponse.class);
 
